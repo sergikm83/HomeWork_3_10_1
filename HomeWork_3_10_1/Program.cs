80d0799451808eb774f1cf865b43f07354662c2d
sergik.m <sergik.m@outlook.com>
2022-04-13 16:34:31 +0400

    определяем четное число или нет выводим результат

--- HomeWork_3_10_1/Program.cs
+++ HomeWork_3_10_1/Program.cs
@@ -8,6 +8,8 @@ namespace HomeWork_3_10_1
         {
             Console.WriteLine("Введите целое число");
             int k = int.Parse(Console.ReadLine());
+            string result = k % 2 == 0 ? "четное" : "не четное";
+            Console.WriteLine($"Число {k} - {result}");
         }
     }
 }
